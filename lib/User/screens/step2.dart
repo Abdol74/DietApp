98706c3fd7d1c7d2d58334c8b7d38b025e402631
yasser <abdoomano85@gmail.com>
2021-05-24 14:50:41 +0200

    update page step 2

--- lib/User/screens/step2.dart
+++ lib/User/screens/step2.dart
@@ -29,7 +29,7 @@ class _StepTwoState extends State<StepTwo> {
   double numberOfCalories;
 
   Color inActiveColor = Colors.grey[200];
-  Color activeColor = Colors.lightGreenAccent;
+  Color activeColor =  Color(0xFF09C04F);
 
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
@@ -39,59 +39,31 @@ class _StepTwoState extends State<StepTwo> {
     return MaterialApp(
       home: Scaffold(
         key: _scaffoldKey,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Row(
-            children: [
-              Container(
-                padding: EdgeInsets.only(right: 100),
-                child: GestureDetector(
-                  onTap: () {
-                    // Navigator.pushNamed(context, WelcomeScreen.id);
-                  },
-                  child: Image.asset(
-                    "assets/images/icons/background.png",
-                    height: 55,
-                    width: 20,
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(right: 20),
-                child: Text(
-                  "Step 2 of 4",
-                  style: TextStyle(color: Colors.green, fontSize: 20),
-                ),
-              ),
-            ],
-          ),
-        ),
-        body: Container(
+        body:
+            ListView(
+        children:<Widget>[
+           Container(
           decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage('assets/images/croppedbackground2.png'),
+              image: AssetImage('assets/images/am1.png'),
               fit: BoxFit.cover,
             ),
           ),
           child: Center(
             child: Column(
               children: <Widget>[
-                Expanded(
-                  child: Container(
-                    child: Center(
-                      child: Text(
-                        "Personal Details ",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey[700],
-                            fontSize: 40),
-                      ),
-                    ),
-                  ),
+                SizedBox(height: 20,),
+                Row(
+                  children:<Widget>[
+                    SizedBox(width: 20,),
+                Icon(
+                  Icons.arrow_back,
+                  color: Color(0xFF09C04F),
+                  size: 30.0,
                 ),
-                SizedBox(
-                  height: 20,
+                 ],
                 ),
+                SizedBox(height: 40,),
                 Padding(
                   padding: EdgeInsets.only(bottom: 200),
                   child: Padding(
@@ -101,6 +73,7 @@ class _StepTwoState extends State<StepTwo> {
                         Container(
                           child: Row(
                             children: <Widget>[
+                              SizedBox(width: 20,),
                               Text(
                                 "Birthday",
                                 style: TextStyle(
@@ -126,12 +99,7 @@ class _StepTwoState extends State<StepTwo> {
                                                   ? DateTime.now()
                                                   : _datetime,
                                               firstDate: DateTime(1996),
-                                              lastDate: DateTime(2025))
-                                          .then((date) {
-                                        setState(() {
-                                          _datetime = date;
-                                        });
-                                      });
+                                              lastDate: DateTime(2025)).then((date) {setState(() {_datetime = date;},);});
                                     },
                                   ),
                                 ),
@@ -142,6 +110,7 @@ class _StepTwoState extends State<StepTwo> {
                         Container(
                           child: Row(
                             children: <Widget>[
+                              SizedBox(width: 20,),
                               Text(
                                 "Height",
                                 style: TextStyle(
@@ -151,8 +120,11 @@ class _StepTwoState extends State<StepTwo> {
                               ),
                               Flexible(
                                 child: Padding(
-                                  padding: EdgeInsets.only(left: 185),
+                                  padding: EdgeInsets.only(left: 180),
                                   child: TextField(
+                                    cursorColor: Colors.green,
+                                    cursorWidth: 4,
+                                    keyboardType: TextInputType.number,
                                     onChanged: (value) {
                                       setState(() {
                                         height = double.parse(value);
@@ -172,6 +144,7 @@ class _StepTwoState extends State<StepTwo> {
                         Container(
                           child: Row(
                             children: <Widget>[
+                              SizedBox(width: 20,),
                               Text(
                                 "Weight",
                                 style: TextStyle(
@@ -181,8 +154,11 @@ class _StepTwoState extends State<StepTwo> {
                               ),
                               Flexible(
                                 child: Padding(
-                                  padding: EdgeInsets.only(left: 185),
+                                  padding: EdgeInsets.only(left: 175),
                                   child: TextField(
+                                    cursorColor: Colors.green,
+                                    keyboardType: TextInputType.number,
+                                    cursorWidth: 4,
                                     onChanged: (value) {
                                       setState(() {
                                         weight = double.parse(value);
@@ -202,23 +178,27 @@ class _StepTwoState extends State<StepTwo> {
                         Container(
                           child: Row(
                             children: <Widget>[
+                              SizedBox(width: 20,),
                               Text(
                                 "age",
                                 style: TextStyle(
                                     fontSize: 20,
                                     color: Colors.grey[700],
-                                    fontWeight: FontWeight.bold),
+                                    fontWeight: FontWeight.bold,),
                               ),
                               Flexible(
                                 child: Padding(
-                                  padding: EdgeInsets.only(left: 140),
+                                  padding: EdgeInsets.only(left: 205),
                                   child: TextField(
+                                    keyboardType: TextInputType.number,
+                                    cursorColor: Colors.green,
+                                    cursorWidth: 4,
                                     onChanged: (value) {
                                       age = double.parse(value);
                                     },
                                     decoration: InputDecoration(
                                         border: InputBorder.none,
-                                        hintText: "0.0kg",
+                                        hintText: "0.0 year",
                                         hintStyle: TextStyle(
                                             fontSize: 20, color: Colors.green)),
                                   ),
@@ -239,16 +219,14 @@ class _StepTwoState extends State<StepTwo> {
                                       : inActiveColor,
                                   elevation: 4.0,
                                   borderRadius: BorderRadius.circular(12.0),
-                                  shadowColor: Colors.grey,
+                                  shadowColor: Colors.grey[500],
                                   child: InkWell(
-                                    splashColor: Colors.redAccent,
+                                    splashColor:  Color(0xFF09C04F),
                                     borderRadius: BorderRadius.circular(16.0),
                                     child: Container(
                                       height: blockVertical * 5.5,
                                       child: Center(
-                                          child: Text(
-                                        "Male",
-                                      )),
+                                          child: Text("Male",)),
                                     ),
                                     onTap: () {
                                       setState(() {
@@ -272,14 +250,12 @@ class _StepTwoState extends State<StepTwo> {
                                   borderRadius: BorderRadius.circular(12.0),
                                   shadowColor: Colors.grey,
                                   child: InkWell(
-                                    splashColor: Colors.redAccent,
+                                    splashColor:  Color(0xFF09C04F),
                                     borderRadius: BorderRadius.circular(16.0),
                                     child: Container(
                                       height: blockVertical * 5.5,
                                       child: Center(
-                                          child: Text(
-                                        "Female",
-                                      )),
+                                          child: Text("Female",)),
                                     ),
                                     onTap: () {
                                       setState(() {
@@ -306,6 +282,7 @@ class _StepTwoState extends State<StepTwo> {
                                   return Container(
                                     margin: EdgeInsets.symmetric(vertical: 40),
                                     height: 35,
+                                    width: 350,
                                     decoration: BoxDecoration(
                                         color: Colors.white,
                                         borderRadius: BorderRadius.circular(20),
@@ -318,8 +295,7 @@ class _StepTwoState extends State<StepTwo> {
                                     child: Row(
                                       children: <Widget>[
                                         Container(
-                                          margin: EdgeInsets.symmetric(
-                                              horizontal: 20.0),
+                                          margin: EdgeInsets.symmetric(horizontal: 20.0),
                                           child: Text(
                                             "Choose Activity",
                                             style: TextStyle(
@@ -328,9 +304,7 @@ class _StepTwoState extends State<StepTwo> {
                                             ),
                                           ),
                                         ),
-                                        Padding(
-                                          padding: EdgeInsets.only(left: 10),
-                                          child: DropdownButton(
+                                           DropdownButton(
                                             elevation: 16,
                                             style: TextStyle(
                                                 color: Colors.grey[800],
@@ -357,7 +331,6 @@ class _StepTwoState extends State<StepTwo> {
                                               });
                                             },
                                           ),
-                                        ),
                                       ],
                                     ),
                                   );
@@ -374,8 +347,9 @@ class _StepTwoState extends State<StepTwo> {
                                   if (!snapshot.hasData) return Container();
 
                                   return Container(
-                                    margin: EdgeInsets.symmetric(vertical: 40),
+                                    margin: EdgeInsets.symmetric(vertical: 10),
                                     height: 30,
+                                    width: 350,
                                     decoration: BoxDecoration(
                                         color: Colors.white,
                                         borderRadius: BorderRadius.circular(20),
@@ -389,17 +363,17 @@ class _StepTwoState extends State<StepTwo> {
                                       children: <Widget>[
                                         Container(
                                           margin: EdgeInsets.symmetric(
-                                              horizontal: 20.0),
+                                              horizontal: 25.0),
                                           child: Text(
-                                            "Choose Activity",
+                                            "Choose Goal",
                                             style: TextStyle(
                                               color: Colors.green,
-                                              fontSize: 14,
+                                              fontSize: 16,
                                             ),
                                           ),
                                         ),
                                         Padding(
-                                          padding: EdgeInsets.only(left: 10),
+                                          padding: EdgeInsets.only(left: 20),
                                           child: DropdownButton(
                                             elevation: 16,
                                             style: TextStyle(
@@ -434,41 +408,16 @@ class _StepTwoState extends State<StepTwo> {
                                 },
                               ),
                             ),
-                            // Container(
-                            //   margin: EdgeInsets.only(top: 2.5),
-                            //   height: 30,
-                            //   width: 250,
-                            //   decoration: BoxDecoration(
-                            //     boxShadow: [
-                            //       BoxShadow(
-                            //           color: Color.fromRGBO(128, 128, 128, 1),
-                            //           blurRadius: 30.0,
-                            //           offset: Offset(0, 20)),
-                            //     ],
-                            //     borderRadius: BorderRadius.circular(70),
-                            //     gradient: LinearGradient(colors: [
-                            //       Color.fromRGBO(9, 180, 78, 1),
-                            //       Color.fromRGBO(9, 180, 78, 1),
-                            //     ]),
-                            //   ),
-                            //   child: Center(
-                            //     child: GestureDetector(
-                            //       onTap: () {
-                            //         // Navigator.pushNamed(context, StepThree.id);
-                            //
-                            //
-                            //       },
-                            //       child: Text(
-                            //         "Next",
-                            //         style: TextStyle(
-                            //             color: Colors.white,
-                            //             fontWeight: FontWeight.bold),
-                            //       ),
-                            //     ),
-                            //   ),
-                            // ),
-
-                            FloatingActionButton(onPressed: () async {
+                            SizedBox(height: 10,),
+                            ElevatedButton(
+                                child: Text('START'),
+                                style: ElevatedButton.styleFrom(
+                                    primary: Color(0xFF09C04F),
+                                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                                    textStyle: TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.bold)),
+                                onPressed: () async {
                               CalculatorBrain calc = CalculatorBrain(
                                   age: age,
                                   height: height,
@@ -491,30 +440,12 @@ class _StepTwoState extends State<StepTwo> {
                                 'goalName': goalValue,
                                 'birthday': _datetime,
                               });
-
-                              Navigator.push(context,
+                                  Navigator.push(context,
                                   MaterialPageRoute(builder: (context) {
                                 return calenderScreen(
                                     //numberOfCalories: numberOfCalories,
                                     );
                               }));
-
-                              // await saveClientData(_currentClient,
-                              //     widget.currentClientId, numberOfCalories);
-
-                              // Navigator.push(
-                              //   context,
-                              //   MaterialPageRoute(
-                              //     builder: (context) => StepThree(
-                              //       totalCalories: calc.totalCalories(),
-                              //       carbs: calc.carb(),
-                              //       protein: calc.protein(),
-                              //       fats: calc.fat(),
-                              //       bmi: calc.bmi(),
-                              //       tdee: calc.tdee(),
-                              //     ),
-                              //   ),
-                              // );
                             }),
                           ],
                         ),
@@ -526,13 +457,12 @@ class _StepTwoState extends State<StepTwo> {
             ),
           ),
         ),
+    ],
       ),
+        ),
     );
   }
 }
-//border: Border(
-//bottom: BorderSide(color: Colors.grey[100]),)
-
 class MyContainerTile extends StatelessWidget {
   const MyContainerTile({this.child, this.height});
 
@@ -554,8 +484,8 @@ class MyContainerTile extends StatelessWidget {
           ),
         ],
       ),
-      padding: EdgeInsets.all(12),
-      margin: EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+      padding: EdgeInsets.all(2),
+      margin: EdgeInsets.symmetric(vertical: 2, horizontal: 1),
       child: child,
     );
   }
